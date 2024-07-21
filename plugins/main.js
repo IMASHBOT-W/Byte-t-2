1ba2fcc62785aeeff8aff14802abc085c75272f7
e9archiotronic <e9@archiotronic.com>
2024-07-21 15:45:51 +0500

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -98,7 +98,7 @@
               }
             }
           }
-          const _0x2eae7c = "👋 Hello " + _0x35e518 + " I'm alive" + "\n*Im X-Byte*\n> *Version:* " + require("../package.json").version + "\n> *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n> *Runtime:* " + runtime(process.uptime()) + "\n> *Platform:* " + hostname;
+          const _0x2eae7c = "👋 Hello " + _0x35e518 + " I'm alive" + "\n*Im X-Byte*\n *Version:* " + require("../package.json").version + "\n *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n *Runtime:* " + runtime(process.uptime()) + "\n *Platform:* " + hostname;
           const _0x15cb75 = {
             display_text: config.BTN,
             url: config.BTNURL,
@@ -175,7 +175,7 @@
               }
             }
           }
-          const _0x2cfda9 = "👋 Hello " + _0x4701d9 + "\n*X-Byte*\n  \n> *Version:* " + require("../package.json").version + "\n> *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n> *Runtime:* " + runtime(process.uptime()) + "\n> *Platform:* " + hostname;
+          const _0x2cfda9 = "👋 Hello " + _0x4701d9 + "\n*X-Byte*\n  \n *Version:* " + require("../package.json").version + "\n *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n *Runtime:* " + runtime(process.uptime()) + "\n *Platform:* " + hostname;
           let _0x5a5ac0 = [{
             'name': "cta_url",
             'buttonParamsJson': JSON.stringify({
@@ -232,7 +232,7 @@
       });
       const _0x542b06 = {
         pattern: "menu2",
-        react: "2️⃣ ", 
+        react: "2️⃣", 
         alias: ["bmenu", "newmenu", "help"],
         desc: "Get bot's command list.",
         category: "main",
@@ -279,7 +279,7 @@
               }
             }
           }
-          const _0x32d4b0 = "〖 X-BYTE 〗*\n> Prefix -"+"["+ xprefix +"]" +  "\n> *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n> *Runtime:* " + runtime(process.uptime());
+          const _0x32d4b0 = "*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime());
           const _0x463791 = [];
           const _0x181fa0 = new Map();
           for (let _0x37434c = 0; _0x37434c < 1; _0x37434c++) {
@@ -343,9 +343,9 @@
               description: "Other commands",
               id: ".othermenu"
             };
+            _0x10cdf6.push(_0x55776a);
             _0x10cdf6.push(_0x41d75d);
             _0x10cdf6.push(_0x48e60c);
-            _0x10cdf6.push(_0x55776a);
             _0x10cdf6.push(_0x5a637b);
             _0x10cdf6.push(_0x24709f);
             _0x10cdf6.push(_0x4c56eb);
@@ -677,7 +677,7 @@
             }
           }
           ;
-          let _0x8e9c18 = "*╭─    〖 X-BYTE 〗*\n│o: Prefix -"+"["+ xprefix +"]"+ "\n*│o:Run Time -* " + runtime(process.uptime()) + " \n*│o:Ram usage -* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n\n\n*╰──────────●●►*\n*X-BYTE COMMANDS*\n> *╭──────────●●►*\n> *│ DOWNLOAD COMMANDS*\n> *│   ───────*\n\n" + _0x1f9061 + "*╰───────────●●►*\n> *╭──────────●●►*\n> *│ SEARCH COMMANDS*\n> *│   ───────*\n\n" + _0x3ea1e0 + "*╰───────────●●►*\n\n> *╭──────────●●►*\n> *│ CONVERT COMMANDS*\n> *│   ───────*\n\n" + _0x2ace9b + "*╰───────────●●►*\n\n> *╭──────────●●►*\n> *│ LOGO COMMANDS*\n> *│   ───────*\n\n" + _0x4046ab + "*╰───────────●●►*\n\n> *╭──────────●●►*\n> *│ MAIN COMMANDS*\n> *│   ───────*\n\n" + _0x9abccf + "*╰───────────●●►*\n\n> *╭──────────●●►*\n> *│ GROUP COMMANDS*\n> *│   ───────*\n\n" + _0x8d8eb5 + "*╰───────────●●►*\n\n> *╭──────────●●►*\n> *│ BUG COMMANDS*\n> *│   ───────*\n\n" + _0x253600 + "*╰───────────●●►*\n\n> *╭──────────●●►*\n> *│ OTHER COMMANDS*\n> *│   ───────*\n\n" + _0x5829ec + "*╰───────────●●►*\t\n *POWERED BY TALKDROVE:)*";
+          let _0x8e9c18 = "*╭──〖 X-BYTE 〗──╮*\n*│o: Prefix:* "+"[ "+ xprefix +" ]"+ "\n*│o:Run Time:* " + runtime(process.uptime()) + " \n*│o:Ram usage:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n*╰──────────●●►*\n*X-BYTE COMMANDS*\n *╭──────────●●►*\n\n\n\n *│ DOWNLOAD COMMANDS*\n\n" + _0x1f9061 + "*╰───────────●●►*\n *╭──────────●●►*\n *│ SEARCH COMMANDS*\n\n" + _0x3ea1e0 + "*╰───────────●●►*\n *╭──────────●●►*\n *│ CONVERT COMMANDS*\n\n" + _0x2ace9b + "*╰───────────●●►*\n *╭──────────●●►*\n *│ LOGO COMMANDS*\n\n" + _0x4046ab + "*╰───────────●●►*\n *╭──────────●●►*\n *│ MAIN COMMANDS*\n\n" + _0x9abccf + "*╰───────────●●►*\n *╭──────────●●►*\n *│ GROUP COMMANDS*\n\n" + _0x8d8eb5 + "*╰───────────●●►*\n *╭──────────●●►*\n *│ BUG COMMANDS*\n\n" + _0x253600 + "*╰───────────●●►*\n *╭──────────●●►*\n *│ OTHER COMMANDS*\n\n" + _0x5829ec + "*╰───────────●●►*\t\n\n *POWERED BY TALKDROVE*";
           const _0x1150b3 = {
             url: config.LOGO
           };
@@ -797,12 +797,12 @@
           for (let _0x5f2522 = 0; _0x5f2522 < commands.length; _0x5f2522++) {
             if (commands[_0x5f2522].category === "download") {
               if (!commands[_0x5f2522].dontAddCommandList) {
-                _0x2abda3 += "*Command :* " + commands[_0x5f2522].pattern + "\n*Desc :* " + commands[_0x5f2522].desc + "\n*Use:* " + commands[_0x5f2522].use + "\n\n";
+                _0x2abda3 += xprefix + commands[_0x5f2522].pattern + "\n*:o * " + commands[_0x5f2522].desc + "\n*:o * " + commands[_0x5f2522].use + "\n\n";
               }
             }
           }
           ;
-          let _0xbb4f82 = "\n> *╭──────────●●►*\n> *│ DOWNLOAD COMMANDS*\n> *│   ───────*\n" + _0x2abda3 + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0xbb4f82 ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ DOWNLOAD COMMANDS*\n\n" + _0x2abda3 + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x454809 = {
             url: config.LOGO
           };
@@ -924,12 +924,12 @@
           for (let _0x1169d8 = 0; _0x1169d8 < commands.length; _0x1169d8++) {
             if (commands[_0x1169d8].category === "search") {
               if (!commands[_0x1169d8].dontAddCommandList) {
-                _0x12ed53 += "*Command :* " + commands[_0x1169d8].pattern + "\n*Desc :* " + commands[_0x1169d8].desc + "\n*Use:* " + commands[_0x1169d8].use + "\n\n";
+                _0x12ed53 += xprefix + commands[_0x1169d8].pattern + "\n*:o * " + commands[_0x1169d8].desc + "\n*:o * " + commands[_0x1169d8].use + "\n\n";
               }
             }
           }
           ;
-          let _0x45eb07 = "\n> *╭──────────●●►*\n> *│ SEARCH COMMANDS*\n> *│   ───────*\n" + _0x12ed53 + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x45eb07 ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ SEARCH COMMANDS*\n\n" + _0x12ed53 + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x41011a = {
             url: config.LOGO
           };
@@ -1042,12 +1042,12 @@
           for (let _0x21bbc8 = 0; _0x21bbc8 < commands.length; _0x21bbc8++) {
             if (commands[_0x21bbc8].category === "convert") {
               if (!commands[_0x21bbc8].dontAddCommandList) {
-                _0x4b2f8d += "*Command :* " + commands[_0x21bbc8].pattern + "\n*Desc :* " + commands[_0x21bbc8].desc + "\n*Use:* " + commands[_0x21bbc8].use + "\n\n";
+                _0x4b2f8d += xprefix + commands[_0x21bbc8].pattern + "\n*:o * " + commands[_0x21bbc8].desc + "\n*:o * " + commands[_0x21bbc8].use + "\n\n";
               }
             }
           }
           ;
-          let _0x269b93 = "\n> *╭──────────●●►*\n> *│ CONVERT COMMANDS*\n> *│   ───────*\n" + _0x4b2f8d + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x269b93 ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ CONVERT COMMANDS*\n\n" + _0x4b2f8d + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x2dfe0a = {
             url: config.LOGO
           };
@@ -1165,12 +1165,12 @@
           for (let _0x281d5c = 0; _0x281d5c < commands.length; _0x281d5c++) {
             if (commands[_0x281d5c].category === "logo") {
               if (!commands[_0x281d5c].dontAddCommandList) {
-                _0x36dfbb += "*Command :* " + commands[_0x281d5c].pattern + "\n*Desc :* " + commands[_0x281d5c].desc + "\n*Use:* " + commands[_0x281d5c].use + "\n\n";
+                _0x36dfbb += xprefix + commands[_0x281d5c].pattern + "\n*:o * " + commands[_0x281d5c].desc + "\n*:o * " + commands[_0x281d5c].use + "\n\n";
               }
             }
           }
           ;
-          let _0x1725f5 = "\n> *╭──────────●●►*\n> *│ LOGO COMMANDS*\n> *│   ───────*\n" + _0x36dfbb + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x1725f5 ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ LOGO COMMANDS*\n\n" + _0x36dfbb + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x472cb0 = {
             url: config.LOGO
           };
@@ -1282,12 +1282,12 @@
           for (let _0x2a7fb3 = 0; _0x2a7fb3 < commands.length; _0x2a7fb3++) {
             if (commands[_0x2a7fb3].category === "main") {
               if (!commands[_0x2a7fb3].dontAddCommandList) {
-                _0x5be7de += "*Command :* " + commands[_0x2a7fb3].pattern + "\n*Desc :* " + commands[_0x2a7fb3].desc + "\n*Use:* " + commands[_0x2a7fb3].use + "\n\n";
+                _0x5be7de += xprefix + commands[_0x2a7fb3].pattern + "\n*:o * " + commands[_0x2a7fb3].desc + "\n*:o * " + commands[_0x2a7fb3].use + "\n\n";
               }
             }
           }
           ;
-          let _0x3e8641 = "\n> *╭──────────●●►*\n> *│ MAIN COMMANDS*\n> *│   ───────*\n" + _0x5be7de + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x3e8641 ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ MAIN COMMANDS*\n\n" + _0x5be7de + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x5592c1 = {
             url: config.LOGO
           };
@@ -1384,12 +1384,12 @@
           for (let _0x2903a7 = 0; _0x2903a7 < commands.length; _0x2903a7++) {
             if (commands[_0x2903a7].category === "group") {
               if (!commands[_0x2903a7].dontAddCommandList) {
-                _0x4568fa += "*Command :* " + commands[_0x2903a7].pattern + "\n*Desc :* " + commands[_0x2903a7].desc + "\n*Use:* " + commands[_0x2903a7].use + "\n\n";
+                _0x4568fa += xprefix + commands[_0x2903a7].pattern + "\n*:o * " + commands[_0x2903a7].desc + "\n*:o * " + commands[_0x2903a7].use + "\n\n";
               }
             }
           }
           ;
-          let _0x56515f = "\n> *╭──────────●●►*\n> *│ GROUP COMMANDS*\n> *│   ───────*\n" + _0x4568fa + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x56515f ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ GROUP COMMANDS*\n\n" + _0x4568fa + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x2e55e9 = {
             url: config.LOGO
           };
@@ -1495,12 +1495,12 @@
           for (let _0x2d3841 = 0; _0x2d3841 < commands.length; _0x2d3841++) {
             if (commands[_0x2d3841].category === "bug") {
               if (!commands[_0x2d3841].dontAddCommandList) {
-                _0x3a953e += "*Command :* " + commands[_0x2d3841].pattern + "\n*Desc :* " + commands[_0x2d3841].desc + "\n*Use:* " + commands[_0x2d3841].use + "\n\n";
+                _0x3a953e += xprefix + commands[_0x2d3841].pattern + "\n*:o * " + commands[_0x2d3841].desc + "\n*:o * " + commands[_0x2d3841].use + "\n\n";
               }
             }
           }
           ;
-          let _0x175040 = "\n> *╭──────────●●►*\n> *│ BUG COMMANDS*\n> *│   ───────*\n" + _0x3a953e + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x175040 ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ BUG COMMANDS*\n\n" + _0x3a953e + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x46bb9f = {
             url: config.LOGO
           };
@@ -1622,12 +1622,12 @@
           for (let _0x2b13c2 = 0; _0x2b13c2 < commands.length; _0x2b13c2++) {
             if (commands[_0x2b13c2].category === "other") {
               if (!commands[_0x2b13c2].dontAddCommandList) {
-                _0x31621a += "*Command :* " + commands[_0x2b13c2].pattern + "\n*Desc :* " + commands[_0x2b13c2].desc + "\n*Use:* " + commands[_0x2b13c2].use + "\n\n";
+                _0x31621a += xprefix + commands[_0x2b13c2].pattern + "\n*:o * " + commands[_0x2b13c2].desc + "\n*:o * " + commands[_0x2b13c2].use + "\n\n";
               }
             }
           }
           ;
-          let _0x537d3b = "\n> *╭──────────●●►*\n> *│ OTHER COMMANDS*\n> *│   ───────*\n" + _0x31621a + "*╰───────────●●►*\nPowered by TalkDrove";
+          let _0x537d3b ="*╭──〖 X-BYTE 〗────╮*\n| *Prefix:* "+"[ "+ xprefix +" ]" +  "\n| *Memory:* " + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB / " + Math.round(require('os').totalmem / 1024 / 1024) + "MB\n| *Runtime:* " + runtime(process.uptime())+ "\n\n *╭──────────●●►*\n *│ OTHER COMMANDS*\n\n" + _0x31621a + "*╰───────────●●►*\nPowered by TalkDrove";
           const _0x3031c7 = {
             url: config.LOGO
           };
